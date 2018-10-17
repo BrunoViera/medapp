62b4585a38d73e524d2cf14e61f525cf26d18690
Bruno Viera <brunovierag@gmail.com>
2018-10-16 21:22:58 -0300

    update environment and configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 const gulp = require('gulp');
+const rev = require('gulp-rev');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('build', ['build:css'], () => {
+  gulp.start('build:addVersion');
 });
+
+gulp.task('build:addVersion', () => gulp
+  .src(['assets/build/**/*', '!assets/build/**/fonts/**', '!assets/build/**/images/**'])
+  .pipe(rev())
+  .pipe(gulp.dest('./public/'))
+  .pipe(rev.manifest('manifest.json'))
+  .pipe(gulp.dest('./public/')));
 
 gulp.task('build:css', () => gulp
   .src('./assets/sass/*.scss')
@@ -12,4 +21,4 @@ gulp.task('build:css', () => gulp
     browsers: ['last 2 versions'],
     cascade: false,
   }))
-  .pipe(gulp.dest('./public/css')));
+  .pipe(gulp.dest('./assets/build/css')));
